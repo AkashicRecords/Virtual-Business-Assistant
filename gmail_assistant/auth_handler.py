da5a3aa2f7c91f8161c5ce81b5355a5232e3663f
Samuel Elder <53106453+AkashicRecords@users.noreply.github.com>
2024-11-21 04:48:23 +0700

    initial commit
    Amended commit to remove secret

--- gmail_assistant/auth_handler.py
+++ gmail_assistant/auth_handler.py
@@ -4,12 +4,19 @@ import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from gmail_assistant.config.secrets import GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_REDIRECT_URI
 
 class AuthHandler:
     def __init__(self):
         self.SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
         self.token_path = 'token.pickle'
-        self.credentials_path = 'credentials.json'
+        self.credentials = {
+            'client_id': GMAIL_CLIENT_ID,
+            'client_secret': GMAIL_CLIENT_SECRET,
+            'redirect_uri': GMAIL_REDIRECT_URI,
+            'auth_uri': "https://accounts.google.com/o/oauth2/auth",
+            'token_uri': "https://oauth2.googleapis.com/token"
+        }
 
     def get_credentials(self):
         creds = None
@@ -21,8 +28,10 @@ class AuthHandler:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.credentials_path, self.SCOPES)
+                flow = InstalledAppFlow.from_client_secrets(
+                    self.credentials, 
+                    self.SCOPES
+                )
                 creds = flow.run_local_server(port=0)
             
             with open(self.token_path, 'wb') as token:
